77edb91f4589df0e64b25e34e548a663ada771f5
Niklas Eicker <nikle1@gmx.de>
2019-04-06 15:07:23 +0200

    simon says is running

--- simon_says/simon.js
+++ simon_says/simon.js
@@ -1,5 +1,36 @@
 function keyDown(e) {
-    playSound(e.keyCode);
+    if(e.keyCode === 32) {
+        if(awaitInput && (currentTile =! currentTiles.length)) return;
+        startRound();
+        return;
+    }
+    const tile = document.querySelector(`.tile[data-key="${e.keyCode}"]`);
+    if(!tile) return;
+    onClick(tile);
+}
+
+function onClick(element) {
+    if(!awaitInput) return;
+    activateTile(element);
+}
+
+function activateTile(element) {
+    if(!element.dataset.key) return;
+    if(awaitInput) {
+        if(!element.dataset.tile) return;
+        if(!(currentTiles[currentTile] == element.dataset.tile)) {
+            lost();
+            return;
+        }
+        currentTile++;
+        if(currentTile === currentTiles.length) {
+            nextRound();
+            rules.innerText = "Press space to start the round";
+            awaitInput = false;
+            currentTile = 0;
+        }
+    }
+    playSound(element.dataset.key)
 }
 
 function playSound(dataKey) {
@@ -12,9 +43,11 @@ function playSound(dataKey) {
     tile.classList.add('clicked');
 }
 
-function onClick(element) {
-    if(!element.dataset.key) return;
-    playSound(element.dataset.key)
+function lost() {
+    rules.innerText = "Lost in round " + currentRound;
+    // block any further actions
+    roundRunning = true;
+    awaitInput = false;
 }
 
 function removeTransition(e) {
@@ -22,21 +55,53 @@ function removeTransition(e) {
     this.classList.remove('clicked');
 }
 
+let roundRunning = false;
+function startRound() {
+    if(roundRunning) return;
+    rules.innerText = "Remember the pattern";
+    roundRunning = true;
+    nextTile();
+}
+
 function nextRound() {
     currentRound++;
     head.innerText = "Round: " + currentRound;
 }
 
-function playCombo() {
-    let count = 0;
-    for roun
+let awaitInput = false;
+let currentRound = 1;
+let currentTile = 0;
+let currentTiles = [];
+function nextTile() {
+    let tileNr;
+    if(currentTile === currentTiles.length) {
+        tileNr = Math.floor((Math.random() * 4) + 1);
+        currentTiles.push(tileNr);
+    } else {
+        tileNr = currentTiles[currentTile];
+    }
+    const tile = document.querySelector(`.tile[data-tile="${tileNr}"]`);
+    activateTile(tile);
+    currentTile ++;
+    if (currentTile >= currentRound) {
+        currentTile = 0;
+        roundRunning = false;
+        awaitInput = true;
+        rules.innerText = "Repeat the pattern";
+        return;
+    }
+    setTimeout(nextTile, 1000);
 }
 
 const tiles = document.querySelectorAll('.tile');
-window.addEventListener('keydown', keyDown);
 tiles.forEach(tile => tile.addEventListener('transitionend', removeTransition));
 
-let currentRound = 1;
-
 const head = document.getElementById("head");
-head.innerText = "Round: " + currentRound;
+const rules = document.getElementById("rules");
+
+function start() {
+    window.addEventListener('keydown', keyDown);
+    head.innerText = "Round: " + currentRound;
+    rules.innerText = "Press space to start the round";
+}
+window.onload = start;
